296a2cd7e2fee652ab2dea045d4cdaabf7d8be6e
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-10 13:50:49 -0300

    Itinerary model

---
+++ src/models/ItineraryModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const { Schema, model } = mongoose;
+
+const ItinerarySchema = new Schema({
+    id: {
+        type: Number
+    },
+    line_objectId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Line'
+    },
+    line_id: {
+        type: Number,
+        required: true
+    },
+    line_code: {
+        type: String,
+        required: true
+    },
+    line_name: {
+        type: String,
+        required: true
+    },
+    itineraries: [
+        {
+            lat: {
+                type: String,
+                required: true
+            },
+            lng: {
+                type: String,
+                required: true
+            },
+        }
+    ]
+}, {
+    timestamps: true //createdAt e updatedAt sao criadas automaticamente
+});
+
+ItinerarySchema.plugin(AutoIncrement, { id:'order_seq', inc_field: 'id' });
+
+module.exports = model('Itinerary', ItinerarySchema);  
