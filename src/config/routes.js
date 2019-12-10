3f57deb16e8e8b814e0616c9c92346183c5ba6bf
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-10 13:35:58 -0300

    make routes API for Line methods

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const LineController = require('../controllers/LineController');
+
 const routes = express.Router();
 
-routes.get('/hello-world', (req, res, next) => {
-    res.send('Hello World!');
-});
+routes.get('/lines', LineController.findAll);
+routes.get('/lines/:id', LineController.findById);
+routes.post('/lines', LineController.store);
+routes.put('/lines/:id', LineController.update);
+routes.delete('/lines/:id', LineController.deleteById);
  
 module.exports = routes;
