60bc0501b4a6384ca6f67b2e3dc15702cb49d526
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-10 13:35:09 -0300

    added LineModel

---
+++ src/models/LineModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const { Schema, model } = mongoose;
+
+const LineSchema = new Schema({
+    id: {
+        type: Number
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true //createdAt e updatedAt sao criadas automaticamente
+});
+
+LineSchema.plugin(AutoIncrement, { id:'order_seq', inc_field: 'id' });
+
+module.exports = model('Dev', LineSchema);
