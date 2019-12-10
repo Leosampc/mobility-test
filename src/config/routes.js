b6893bd3f603374073f3430c44139faf1946658c
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-10 15:43:14 -0300

    added API routes for itinerary methods

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const LineController = require('../controllers/LineController');
+const ItineraryController = require('../controllers/ItineraryController');
 
 const routes = express.Router();
 
@@ -9,5 +10,8 @@ routes.get('/lines/:id', LineController.findById);
 routes.post('/lines', LineController.store);
 routes.put('/lines/:id', LineController.update);
 routes.delete('/lines/:id', LineController.deleteById);
+
+routes.post('/itineraries', ItineraryController.store);
+routes.put('/itineraries/:line_id', ItineraryController.update);
  
 module.exports = routes;
