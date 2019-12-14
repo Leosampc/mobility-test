53d9826893e2f88b821b08ddfa03d4975e13bc6c
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-14 14:25:05 -0300

    change models to schemas

---
+++ src/schemas/Line.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const { Schema, model } = mongoose;
+
+const LineSchema = new Schema({
+    id: {
+        type: Number
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true //createdAt e updatedAt sao criadas automaticamente
+});
+
+LineSchema.plugin(AutoIncrement, { id:'line_id', inc_field: 'id' });
+
+module.exports = model('Line', LineSchema);
