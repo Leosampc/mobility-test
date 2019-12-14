4db9c522793c97f40cb4e6461715ee3fabd669a8
leonardo-lighthouse <leonardo.cruz@lighthouseit.com.br>
2019-12-14 14:27:49 -0300

    make app class

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-require('./config/server');
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+require('./database');
+
+class App {
+    constructor() {
+        this.server = express();
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.server.use(express.json());
+        this.server.use(cors());
+    }
+
+    routes() {
+        this.server.use(routes);
+    }
+}
+
+module.exports = new App().server;
